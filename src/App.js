b7594bc1f39046daf236a1987bdb815c89dc5661
Borjaarmet <borjaarmet@gmail.com>
2022-01-21 11:57:41 +0100

    change default value

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#004465").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,6 +27,7 @@ function App() {
         <form onSubmit={handleSubmit}>
           <input
             type="text"
+            placeholder="#004465"
             value={color}
             onChange={(e) => {
               setColor(e.target.value);
