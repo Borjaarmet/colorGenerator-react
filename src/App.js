f2fcaad5b66ce6c00302aaa96cc8feadedf2358d
Borjaarmet <borjaarmet@gmail.com>
2022-01-21 10:21:48 +0100

    setup structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+          ></input>
+          <button className="btn">Generate</button>
+        </form>
+      </section>
+      <section>
+        <h3>list of colors</h3>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
