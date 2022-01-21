66ad20dac0a92d0f75ec09f8d69f0aa6b7549776
Borjaarmet <borjaarmet@gmail.com>
2022-01-21 11:53:36 +0100

    copy to clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -7,13 +7,27 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bcg = rgb.join(",");
   // const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false);
+    }, 2500);
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [alert]);
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
